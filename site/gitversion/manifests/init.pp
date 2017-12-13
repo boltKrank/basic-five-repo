fd04788d26736350541bda2b3bb9b6f73924f03b
Simon Anderson <simon.anderson111@gmail.com>
2017-12-14 10:39:53 +1100

    updated git_version

---
+++ site/gitversion/manifests/init.pp
@@ -0,0 +1,5 @@
+class gitversion {
+
+  notify { "${facts['git_version']" : }
+
+}
---
+++ site/profile/manifests/basicfiveprofile.pp
@@ -0,0 +1,10 @@
+class profile::basicfiveprofile {
+
+    # Insert implementation here:
+    # A module to manage Apache and MySQL with 5 website and 5 databases.
+    # eyaml for passwords
+
+    # A module that creates a fact to return the current version of git installed.
+    include gitversion
+
+}
