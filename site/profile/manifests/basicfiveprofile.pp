c1cb7c1493d70e95c9ed4b394bd0e7777cd3c50f
Your Name <you@example.com>
2018-01-16 04:05:03 +0000

    hiera update

--- site/profile/manifests/basicfiveprofile.pp
+++ site/profile/manifests/basicfiveprofile.pp
@@ -1,4 +1,45 @@
-class profile::basicfiveprofile {
+class profile::basicfiveprofile (
+  String $rootpassword = lookup('profile::basicfiveprofile::rootpassword', 'strongpassword'),
+  Array $vhosts = lookup('profile::basicfiveprofile::vhosts', ['vhost1','vhost2']),
+){
+  class { '::mysql::server':
+    root_password           => $rootpassword,
+    remove_default_accounts => true,
+    override_options        => $override_options,
+    databases               => {  
+    'db1'  => {
+        ensure  => 'present',
+        charset => 'utf8',
+      },
+      'db2'  => {
+        ensure  => 'present',
+        charset => 'utf8',
+      },
+      'db3'  => {
+        ensure  => 'present',
+        charset => 'utf8',
+      },
+      'db4'  => {
+        ensure  => 'present',
+        charset => 'utf8',
+      },
+      'db5'  => {
+        ensure  => 'present',
+        charset => 'utf8',
+      },
+    }
+
+  }
+  
+  include apache
+
+  $vhosts = ['vhost1','vhost2','vhost3','vhost4','vhost5']
+
+  $vhosts.each |String $vhost| {
+    apache::vhost { $vhost:
+      port    => '80',
+      docroot => "/var/www/${vhost}",
+  }
 
     # Insert implementation here:
     # A module to manage Apache and MySQL with 5 website and 5 databases.
